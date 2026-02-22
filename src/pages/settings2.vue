58540d6e60d2e6048deca4e6bf84c13f1e2fc0b4
William Sampaio <56176702+WilliamSampaio@users.noreply.github.com>
2026-02-22 02:40:50 -0400

    feat: refatorar painel de configurações com melhorias na exibição e lógica de estado

--- src/pages/settings2.vue
+++ src/pages/settings2.vue
@@ -56,112 +56,145 @@
             <v-card prepend-icon="mdi-panorama-outline" title="Painel" subtitle="Configurações do Painel.">
               <v-divider></v-divider>
 
-              <v-card-text class="flex items-center justify-center bg-grey-lighten-4 py-10">
-                <v-sheet elevation="1" width="640px" height="360px" class="overflow-hidden relative flex flex-col"
-                  rounded="lg">
+              <v-card-text class="pa-0">
 
-                  <!-- HEADER -->
-                  <v-sheet height="80px">
-                    <div class="flex h-full px-5 items-center justify-between [&>*:only-child]:mx-auto"
-                      :style="{ backgroundColor: header.bgColor, color: header.textColor }">
+                <v-row>
 
-                      <img v-if="panelStore.headerLeftLogoUrlIsDefined" class="w-24" :src="header.leftLogoUrl" />
+                  <v-col cols="8">
 
-                      <span class="text-2xl font-bold uppercase">minha unidade</span>
+                    <v-sheet class="flex items-center justify-center bg-grey-lighten-4 py-10">
 
-                    </div>
-                  </v-sheet>
+                      <v-sheet elevation="1" width="640px" height="360px" class="overflow-hidden relative flex flex-col"
+                        rounded="lg">
 
-                  <!-- MAIN -->
-                  <v-sheet height="200px">
-                    <div class="flex flex-1 h-full py-2 items-center justify-between bg-white"
-                      :style="{ backgroundColor: main.bgColor }">
+                        <!-- HEADER -->
+                        <v-sheet height="80px">
+                          <div class="flex h-full px-5 items-center justify-between [&>*:only-child]:mx-auto"
+                            :style="{ backgroundColor: header.bgColor, color: header.textColor }">
 
-                      <!-- CURRENT TICKET -->
-                      <div class="ml-3 flex w-3/4 h-full flex-col justify-between">
+                            <img v-if="panelStore.headerLeftLogoUrlIsDefined" class="w-24" :src="header.leftLogoUrl" />
 
-                        <span class="text-1xl font-bold uppercase" :style="{ color: main.ticketLabelColor }">
-                          {{ ticketLabel }}
-                        </span>
-
-                        <div class="flex flex-col">
-
-                          <span class="text-4xl font-bold uppercase" :style="{ color: main.ticketColor }">
-                            A007
-                          </span>
-
-                          <span class="text-2xl font-bold uppercase" :style="{ color: main.ticketColor }">
-                            james bond
-                          </span>
-
-                        </div>
-
-                        <span class="text-2xl font-bold uppercase" :style="{ color: main.serviceColor }">
-                          descição do serviço
-                        </span>
-
-                        <span class="text-4xl font-bold uppercase" :style="{ color: main.localColor }">
-                          guichê 1
-                        </span>
-
-                      </div>
-
-                      <!-- HISTORY -->
-                      <v-sheet class="px-3 flex w-1/4 h-full flex-col items-center rounded-l-3xl"
-                        :style="{ backgroundColor: main.historyBgColor }">
-
-                        <span class="text-xs font-bold uppercase" :style="{ color: main.historyLabelColor }">
-                          {{ $t('panel.history.title') }}
-                        </span>
-
-                        <span v-if="fakeHistory.length === 0" class="text-2xl font-bold uppercase"
-                          :style="{ color: main.historyEmptyColor }">
-                          {{ $t('panel.empty') }}
-                        </span>
-
-                        <div class="w-full h-full flex flex-col items-center justify-around" v-else>
-
-                          <div v-for="ticket in fakeHistory" :key="ticket.id" class="flex flex-col text-center" :style="{
-                            color: ticket.priority ? main.ticketPriorityColor : main.ticketColor
-                          }">
-
-                            <span class="text-2xl font-bold uppercase">
-                              {{ ticket.ticket }}
-                            </span>
-
-                            <span v-if="main.historyShowLocal" class="font-bold uppercase">
-                              {{ ticket.local }}
-                            </span>
+                            <span class="text-2xl font-bold uppercase">{{ panelDataPreview.unityDescription }}</span>
 
                           </div>
+                        </v-sheet>
 
-                        </div>
+                        <!-- MAIN -->
+                        <v-sheet height="200px">
+                          <div class="flex flex-1 h-full py-2 items-center justify-between bg-white"
+                            :style="{ backgroundColor: main.bgColor }">
+
+                            <!-- CURRENT TICKET -->
+                            <div class="ml-3 flex w-3/4 h-full flex-col justify-between">
+
+                              <span class="text-1xl font-bold uppercase" :style="{ color: main.ticketLabelColor }">
+                                {{ ticketLabel }}
+                              </span>
+
+                              <div class="flex flex-col">
+
+                                <span class="text-4xl font-bold uppercase" :style="{ color: ticketColor }">
+                                  {{ panelDataPreview.message.ticket }}
+                                </span>
+
+                                <span v-if="!panelState.hideClientName" class="text-2xl font-bold uppercase"
+                                  :style="{ color: ticketColor }">
+                                  {{ panelDataPreview.message.clientName }}
+                                </span>
+
+                              </div>
+
+                              <span class="text-2xl font-bold uppercase" :style="{ color: main.serviceColor }">
+                                {{ panelDataPreview.message.service }}
+                              </span>
+
+                              <span class="text-4xl font-bold uppercase" :style="{ color: main.localColor }">
+                                {{ panelDataPreview.message.local }}
+                              </span>
+
+                            </div>
+
+                            <!-- HISTORY -->
+                            <v-sheet class="px-3 flex w-1/4 h-full flex-col items-center rounded-l-3xl"
+                              :style="{ backgroundColor: main.historyBgColor }">
+
+                              <span class="text-xs font-bold uppercase" :style="{ color: main.historyLabelColor }">
+                                {{ $t('panel.history.title') }}
+                              </span>
+
+                              <span v-if="panelDataPreview.history.length === 0" class="text-2xl font-bold uppercase"
+                                :style="{ color: main.historyEmptyColor }">
+                                {{ $t('panel.empty') }}
+                              </span>
+
+                              <div class="w-full h-full flex flex-col items-center justify-around" v-else>
+
+                                <div v-for="ticket in panelDataPreview.history" :key="ticket.id"
+                                  class="flex flex-col text-center" :style="{
+                                    color: ticket.priority ? main.ticketPriorityColor : main.ticketColor
+                                  }">
+
+                                  <span class="text-2xl font-bold uppercase">
+                                    {{ ticket.ticket }}
+                                  </span>
+
+                                  <span v-if="main.historyShowLocal" class="font-bold uppercase">
+                                    {{ ticket.local }}
+                                  </span>
+
+                                </div>
+
+                              </div>
+
+                            </v-sheet>
+
+                          </div>
+                        </v-sheet>
+
+                        <!-- FOOTER -->
+                        <v-sheet height="80px">
+                          <div class="flex h-full px-5 items-center justify-between [&>*:only-child]:mx-auto"
+                            :style="{ backgroundColor: footer.bgColor, color: footer.textColor }">
+
+                            <img v-if="panelStore.footerLeftLogoUrlIsDefined" class="w-24" :src="footer.leftLogoUrl" />
+
+                            <div class="text-1xl text-white text-center font-medium">
+
+                              <clock v-if="footer.showClock" :locale="mainStore.config.locale"
+                                :dateFormat="$t('date_format')" />
+
+                            </div>
+
+                            <img v-if="panelStore.footerRightLogoUrlIsDefined" class="w-24"
+                              :src="footer.rightLogoUrl" />
+
+                          </div>
+                        </v-sheet>
 
                       </v-sheet>
 
-                    </div>
-                  </v-sheet>
+                    </v-sheet>
 
-                  <!-- FOOTER -->
-                  <v-sheet height="80px">
-                    <div class="flex h-full px-5 items-center justify-between [&>*:only-child]:mx-auto"
-                      :style="{ backgroundColor: footer.bgColor, color: footer.textColor }">
+                  </v-col>
 
-                      <img v-if="panelStore.footerLeftLogoUrlIsDefined" class="w-24" :src="footer.leftLogoUrl" />
+                  <v-col cols="4">
 
-                      <div class="text-1xl text-white text-center font-medium">
+                    <v-switch v-model="panelState.empty" label="Painel vazio" hide-details inset color="primary"
+                      :disabled="panelState.hideClientName === true || panelState.priority === true || panelState.historyEmpty === true"></v-switch>
 
-                        <clock v-if="footer.showClock" :locale="mainStore.config.locale"
-                          :dateFormat="$t('date_format')" />
+                    <v-switch v-model="panelState.hideClientName" label="Ocultar nome do cliente" hide-details inset
+                      color="primary" :disabled="panelState.empty === true"></v-switch>
 
-                      </div>
+                    <v-switch v-model="panelState.priority" label="Senha atual prioridade" hide-details inset
+                      color="primary" :disabled="panelState.empty === true"></v-switch>
 
-                      <img v-if="panelStore.footerRightLogoUrlIsDefined" class="w-24" :src="footer.rightLogoUrl" />
+                    <v-switch v-model="panelState.historyEmpty" label="Histórico vazio" hide-details inset
+                      color="primary" :disabled="panelState.empty === true"></v-switch>
 
-                    </div>
-                  </v-sheet>
+                  </v-col>
 
-                </v-sheet>
+                </v-row>
+
               </v-card-text>
             </v-card>
           </v-col>
@@ -172,7 +205,7 @@
 </template>
 
 <script setup>
-import { computed, ref } from 'vue'
+import { computed, reactive, ref } from 'vue'
 import { usePanelStore } from '@/stores/panel'
 import Clock from '@/components/Clock.vue'
 import { useMainStore } from '@/stores/main'
@@ -183,34 +216,70 @@ const { t } = useI18n()
 const drawer = ref(true)
 const rail = ref(false)
 
+const panelState = reactive({
+  empty: false,
+  hideClientName: false,
+  priority: false,
+  historyEmpty: false,
+})
+
 const panelStore = usePanelStore()
 const { header, main, footer } = panelStore
 
 const mainStore = useMainStore()
 
-const ticketLabel = computed(() => {
-  return mainStore.message.priority ? `${t('panel.ticket')} (${t('panel.priority')})` : t('panel.ticket')
+const panelDataPreview = computed(() => {
+
+  const data = {
+    unityDescription: panelState.empty ? '---' : 'minha unidade',
+    message: {
+      ticket: panelState.empty ? '---' : 'A007',
+      clientName: panelState.empty ? '---' : 'james bond',
+      service: panelState.empty ? '---' : 'descrição do serviço',
+      local: panelState.empty ? '---' : 'guichê 1',
+      priority: panelState.priority
+    },
+    history: [
+      {
+        id: 1,
+        ticket: 'A001',
+        local: 'guichê 1',
+        priority: false
+      },
+      {
+        id: 2,
+        ticket: 'A002',
+        local: 'guichê 2',
+        priority: true
+      },
+      {
+        id: 3,
+        ticket: 'A003',
+        local: 'guichê 3',
+        priority: false
+      }
+    ]
+  }
+
+  if (panelState.empty || panelState.historyEmpty) {
+    data.history = []
+  }
+
+  return data
 })
 
-const fakeHistory = [
-  {
-    id: 1,
-    ticket: 'A001',
-    local: 'guichê 1',
-    priority: false
-  },
-  {
-    id: 2,
-    ticket: 'A002',
-    local: 'guichê 2',
-    priority: true
-  },
-  {
-    id: 3,
-    ticket: 'A003',
-    local: 'guichê 3',
-    priority: false
-  }
-]
+const ticketLabel = computed(() => {
+  return panelState.priority ? `${t('panel.ticket')} (${t('panel.priority')})` : t('panel.ticket')
+})
+
+const ticketColor = computed(() => {
+  return panelState.priority ? main.ticketPriorityColor : main.ticketColor
+})
+
+// watch(panelState, () => {
+//   panelStore.setHeader(panelDataPreview.value.header)
+//   panelStore.setMain(panelDataPreview.value.main)
+//   panelStore.setFooter(panelDataPreview.value.footer)
+// }, { deep: true, immediate: true })
 
 </script>
