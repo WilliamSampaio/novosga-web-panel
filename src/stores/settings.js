93e3c74c564b0c3a02ff23a301365c9903c4d3e0
William Sampaio <56176702+WilliamSampaio@users.noreply.github.com>
2026-02-26 20:54:22 -0400

    feat: adicionar mensagens de erro ao buscar serviços na configuração

--- src/stores/settings.js
+++ src/stores/settings.js
@@ -3,6 +3,7 @@ import { Client as Client21 } from '@/composables/api/21'
 import { useServerStore } from './server'
 import { useAuthStore } from './auth'
 import storage from '@/composables/storage'
+import { useMessagesStore } from './messages'
 
 export const useSettingsStore = defineStore('settings', {
   state: () => {
@@ -47,6 +48,15 @@ export const useSettingsStore = defineStore('settings', {
       const authStore = useAuthStore()
       const api = new Client21(serverStore.apiUrl, null, serverStore.apiRetries)
       const data = await api.services(authStore.accessToken, unityId)
+
+      const messages = useMessagesStore()
+
+      if (!Array.isArray(data) || data.length === 0) {
+        this.services = []
+        messages.add({ text: 'Nenhum serviço enocontrado!', color: 'error' })
+        return
+      }
+
       this.services = data
     },
   },
